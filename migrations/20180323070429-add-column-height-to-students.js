756d788b5625c7138c92e12aec87dbcec0d8dd65
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-03-23 20:00:54 +0700

    sudah

---
+++ migrations/20180323070429-add-column-height-to-students.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.addColumn('Students', 'height', {type: Sequelize.STRING})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('Students', 'height', {type: Sequelize.STRING})
+  }
+};
---
+++ migrations/20180323100830-change-type-data-heigth-to-string-on-students.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('Students', 'height', {type: Sequelize.INTEGER})
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.removeColumn('Students', 'height', {type: Sequelize.STRING})
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
